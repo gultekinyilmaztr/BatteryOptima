b3150ac14048ce714f7c0c608427b02ae1bee6d2
Gültekin YILMAZ <gultekinyilmaz@hotmail.com.tr>
2024-05-15 17:27:13 +0300

    PartialSideBar&Scripts

---
+++ Frontend/BatteryOptima.WebUI/Controllers/BatteryOptimaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BatteryOptima.WebUI.Controllers
+{
+    public class BatteryOptimaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
