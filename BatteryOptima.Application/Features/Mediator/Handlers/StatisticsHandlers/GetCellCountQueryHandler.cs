a17305e79cabef5da80f6cfbbcd0fcf872e387b7
Gültekin YILMAZ <gultekinyilmaz@hotmail.com.tr>
2024-06-06 16:59:20 +0300

    CellCount

---
+++ BatteryOptima.Application/Features/Mediator/Handlers/StatisticsHandlers/GetCellCountQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using BatteryOptima.Application.Features.Mediator.Queries.StatisticsQueries;
+using BatteryOptima.Application.Features.Mediator.Results.StatisticsResults;
+using BatteryOptima.Application.Interfaces.BatteryCellInterfaces;
+using BatteryOptima.Application.Interfaces.CellDetailInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatteryOptima.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetCellCountQueryHandler : IRequestHandler<GetCellCountQuery, GetCellCountQueryResult>
+    {
+        private readonly IBatteryCellRepository _repository;
+        public async Task<GetCellCountQueryResult> Handle(GetCellCountQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetCellCount();
+            return new GetCellCountQueryResult
+            {
+                CellCount = value,
+            };
+
+        }
+    }
+}
